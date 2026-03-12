4443bcfd3f07f1b2f8dc4e9e273d959d2c4e1de6
yawwnann <c0478592@gmail.com>
2026-03-13 00:08:26 +0700

    fix: configure helmet middleware with cross-origin resource policy

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -26,7 +26,11 @@ app.use(
     credentials: true,
   }),
 );
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+  }),
+);
 
 app.set("trust proxy", 1);
 
