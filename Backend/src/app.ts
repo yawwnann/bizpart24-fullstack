273ead06802bb11c685174d7be5b836beea72731
yawwnann <c0478592@gmail.com>
2026-03-12 23:38:53 +0700

    fix: update CORS origin to use production URL for frontend

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin:
+      process.env.FRONTEND_URL || "https://bizpart24-fullstack.vercel.app/",
     credentials: true,
   }),
 );
