1e321fec403f43d06c9b474345ff2901384d10fd
yawwnann <c0478592@gmail.com>
2026-03-12 23:49:11 +0700

    fix: add trust proxy setting for improved request handling

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -19,6 +19,8 @@ app.use(
 );
 app.use(helmet());
 
+app.set("trust proxy", 1);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
